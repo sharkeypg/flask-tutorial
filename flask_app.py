6bfb326b14308dcd31e27dc75888878cb63ef9eb
Paul Sharkey <paul.sharkey01@bbc.co.uk>
2019-10-02 11:11:01 +0100

    Pandas

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,6 @@
 # flask_app.py
-from flask import Flask
+from flask import Flask, jsonify
+import pandas as pd
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -8,3 +9,9 @@ app.config["DEBUG"] = True
 @app.route("/")
 def hello():
     return "Goodbye ≈Earth until next time!"
+
+data = {'a':1, 'b':2}
+
+@app.route("/api")
+def json_ify():
+    return jsonify(data)
